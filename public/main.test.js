4a4b4d1d563e640a0c194bc6ea1f2b0bc9ef0218
craig-dsilva <craig.s.dsilva@gmail.com>
2023-04-24 17:01:18 +0100

    adjective test

--- public/main.test.js
+++ public/main.test.js
@@ -1,21 +1,42 @@
 const fetchDescription = require("./fetchDescription");
 
 const cityName = "London";
+const adjective = "well";
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
-    json: () => Promise.resolve(`${cityName} is well`),
+    json: () => Promise.resolve(`${cityName} is ${adjective}`),
   })
 );
 
 describe("City test", () => {
   test("Should be a string", async () => {
     const sentence = await fetchDescription();
+
     expect(typeof sentence).toBe("string");
   });
 
-  test("City name should be uppercase", async () => {
+  test("City name should begin with uppercase", async () => {
     const sentence = await fetchDescription();
-    expect(sentence.split("")[0]).toBe(cityName.split("")[0].toUpperCase());
+    const cityFirstLetter = cityName.split("")[0].toUpperCase();
+
+    expect(sentence.split("")[0]).toBe(cityFirstLetter);
+  });
+});
+
+describe("Adjective test", () => {
+  test("Should be a string", async () => {
+    const sentence = await fetchDescription();
+
+    expect(typeof sentence).toBe("string");
+  });
+
+  test("Adjective should begin with lowercase", async () => {
+    const sentence = await fetchDescription();
+    const adjectiveIndex =
+      sentence.split("")[sentence.length - adjective.length];
+    const adjectiveFirstLetter = adjective.split("")[0].toLowerCase();
+
+    expect(adjectiveIndex).toBe(adjectiveFirstLetter);
   });
 });
